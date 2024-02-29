75086dbe024b924acfe0613b0399ed5a0d936819
Ruhan Odendaal <ruhan.odendaal@investec.co.za>
2024-02-29 12:56:13 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { giga } from 'gigachat';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-personal-feature';
+  transactionData = undefined
+  htmlResponse:any = ''
+  constructor() {
+    giga.platform.init().then(() => giga.platform.initSession())
+
+    giga.ai.gigaChat.open()
+
+    this.getTransactionData().then(res => {
+      this.transactionData = res
+    })
+  }
+
+  async getTransactionData() {
+    try {
+      const accountList = await giga.api.privateClient.pbsa.accounts.list();
+      const transactions = await giga.api.privateClient.pbsa.transactions.get({
+        accountId: accountList.result.PrivateBankAccounts[0].AccountNumberForRequests,
+        dateFrom: '08/10/2022',
+        dateTo: '08/11/2022'
+      });
+      return transactions.result;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  executeAiMessage(){
+    const prompt = 'Analyse the following transactions for a month and explain what i am spending most of my money on and provide any recommendations on what i can do to better manage my finances. \n' +
+      ' transaction data:\n' +
+      ' \n' + JSON.stringify(this.transactionData) +
+      ' Return the answer in html'
+
+    giga.ai.message.add(prompt).then(res => {
+      console.log(res)
+      this.htmlResponse = res;
+    })
+  }
+
 }
+
+
